5c90081da7c0d29ff47bbc6d5ca6496509e3bbde
Kent Johnric Sardina <Sardinakent08@gmail.com>
2020-12-29 18:34:51 +0800

    Dec.29,2020-0634

---
+++ #17_for_loop.py
@@ -0,0 +1,19 @@
+#for letter in "Giraffe Academy":
+    #print(letter)
+
+friends = ["Kent", "Chow", "Rod"]
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("number: " + str(index))
+
+#for friend in friends:
+#    print(friend)
+
+#for index in range(3, 10):
+#    print(index)
